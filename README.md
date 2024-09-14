20310ed6a3fbfa332ff616d95f4813ae763fef4c
Anu Heese <anuheese@gmail.com>
2024-09-14 20:18:30 +0100

    clicker, api data, some css

--- README.md
+++ README.md
@@ -5,8 +5,19 @@ Please also provide an assignment reflection in your project README.md file.
 # Required
 
 -What requirements did you achieve?- Were there any requirements or goals that you were unable to achieve?
+
+I was able to add an event listener and handler to the cookie button which showed how many clicks the user had made.
+I was also able to get the data from the API and parse it to the page.
+
+I was unable to add a timer to the cookies per second.
+
+I was unable to log the cookies count to the local storage and save this so that the user does not loose their cookie count when they close the page.
+
+I was also unable to make the buy buttons work and add the amount to cookies pers second to change how many cookies the user gets per click.
+
 -If so, what was it that you found difficult about these tasks?
-Optional
+I do not have a good understanding of JS. I know what I want to achieve but my basic foundation of JS is not good enough to implement any of this. Trying to figure this out on my own without a lot of external help was next to impossible. Spending hours googling, going over the workshops and reading the JS book I bought was not sufficient and for my own mental health I did not want to continue as it was making me feel terrible. I am hopeful with more exposure I can get better and I will continue reading the book on JS I got which has been very helpful in solidifying some concepts but I have only covered the first two chapters. I am going to leave the cookie clicker here for now.
+
 🏹 Feel free to add any other reflections you would like to share about your submission, for example:
 
 Requesting feedback about a specific part of your submission.
--- app.js
+++ app.js
@@ -1,1 +1,99 @@
 console.log("test");
+
+let cookieCount = 0;
+let cookiesPerSecond = 1;
+let shopUpgrades = [];
+
+const cookieCountContainer = document.getElementById("cookie-count");
+console.log(cookieCountContainer);
+
+const cookieButton = document.getElementById("cookie-button");
+console.log(cookieButton);
+
+const cookiesPerSecondContainer = document.getElementById(
+  "cookies-per-second-container"
+);
+console.log(cookiesPerSecondContainer);
+
+const cookiesPerSecondValue = document.getElementById(
+  "cookies-per-second-value"
+);
+console.log(cookiesPerSecondValue);
+
+const shopUpgradesContainer = document.getElementById("shop-upgrades");
+
+function displayCookiesPerSecondValue() {
+  cookiesPerSecondValue.content = cookiesPerSecond;
+}
+//this function logs the amount of times the user clicks the cookie button/image and displays it on the html
+function handleClick() {
+  cookieCount = cookieCount + 1;
+  cookieCountContainer.textContent =
+    "You have clicked " + cookieCount + " cookies!";
+  console.log(cookieCount);
+}
+
+cookieButton.addEventListener("click", handleClick);
+
+function handleBuyClick(event) {
+  console.log("buy!");
+  console.log(event.target.id);
+  let buttonId = event.target.id;
+
+  // Step 1: Get the id of the button that was clicked
+  // let buttonId = ...
+
+  // Step 2: Get the object from shopUpgrades with the same id as the button that was clicked
+  //         (call a function created outside this one - suggestion: call it getObjectWithId(id))
+  // let object = ...
+
+  // Step 3: Use the object to add `increase` to fliesPerSecond (if the user has enough cookieCount)
+  //         If they did, also subtract the `cost` from cookieCount
+
+  // Step 4: Update the text of cookieCountContainer and fliesPerSecondContainer
+}
+
+function getObjectWithId(id) {
+  shopUpgrades.find((i) => {
+    return;
+  });
+  //   // TODO: complete this function
+  // this function should use the shopUpgrades array and return an object from the array
+  // if its id is the same as the id passed in as a parameter to this function
+  // hint: you will need a for loop and ifs / elses
+  // return ...
+}
+
+//this function uses the data from the API and renders it onto the page
+function renderShopUpgrades() {
+  shopUpgrades.forEach((item) => {
+    let makeDiv = document.createElement("div");
+    let buyButton = document.createElement("button");
+    buyButton.textContent = "Buy";
+    buyButton.addEventListener("click", handleBuyClick);
+    buyButton.id = `${item.id}`;
+    makeDiv.textContent = `${item.name},Item Cost ${item.cost},Cookies Per Second ${item.increase}`;
+    makeDiv.className = "upgrades-box";
+    shopUpgradesContainer.appendChild(makeDiv);
+    shopUpgradesContainer.appendChild(buyButton);
+  });
+}
+//this function fetches the data from the API and console logs it
+async function getShopUpgrades() {
+  const response = await fetch(
+    "https://cookie-upgrade-api.vercel.app/api/upgrades"
+  );
+  console.log(response);
+  const shopData = await response.json();
+  console.log(shopData);
+  shopUpgrades = shopData;
+  console.log(shopUpgrades);
+  renderShopUpgrades();
+}
+//this function makes  getShopUgrades() wait before
+async function main() {
+  await getShopUpgrades();
+  console.log(shopUpgrades);
+}
+
+main();
