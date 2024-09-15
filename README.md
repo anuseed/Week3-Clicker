889e7c9c145cf55be462e756d247e0bc46a37aff
Anu Heese <anuheese@gmail.com>
2024-09-15 21:34:34 +0100

    final final

--- README.md
+++ README.md
@@ -13,17 +13,16 @@ I added a setInterval to the cookie counter to keep adding 1 cookie per second.
 
 I used the local storage workshop to help me understand how to log the cookies count to the local storage and save this so that the user does not loose their cookie count when they close the page.I also encountered an error which returned null when the user first uses the cookie clicker as there is no cookie count stored in local storage.
 
-I made a reset button which cleared the cookie count.
+I made a reset button which cleared the cookie count and set cookiesPerSecond back to 1.
 
-I was also unable to make the buy buttons work and add the amount to cookies pers second to change how many cookies the user gets per click. My husband gave me a really good lesson in for loops and finding objects in the array (his commments are in the js) but I had to give up after a point as I just wasn't able to get it to work. The whole process just felt incredibly overwhelming.
+Using the id from the buy button to add buy items and add value to the cookiesPerSecond value was an absolute rollercoaster. I really struggled with this one! My husband gave me some really good basic lessons in loops and finding items in an array, but super tricky for me to get my head around. Really happy it finally works.
 
-Styling elements created in JS(from the Shop Ugrades) was very tricky, I didn't know how to reference these items in css. Creating a div around the element in HTML made it all dissapear so I left it as is.
+Styling elements created in JS(from the Shop Ugrades) was very tricky, I didn't know how to reference these items in css. I was busy trying to style the media queries but messed up my page and now have run out of time. Will see if I can fix this before the deadline.
 
-I have purposefully left this assignment here rather than ask for more help to avoid doing something I don't really understand. I feel it is important to acknowledge that this is the extent of my knowledge currently and rather than spend more time on this I will try and finish reading the JS book I got, going over examples and so on.
+This was an incredibly hard assignment for me, I didn't enjoy a lot of it. It made me feel like I just didn't get anything. I liked doing the CSS and enjoy styling the page but the JS was hard. I am very grateful to have someone around the house to help me understand concepts and occasionaly debug but without which I would have really struggled to get any of this done.
 
 -If so, what was it that you found difficult about these tasks?
-I have a lot of gaps in my understanding of JS, this makes it really difficult to know how to start, comments and instructions given in the skeleton where useful to an extent but for some aspects I had too many gaps in my understanding to complete them. I know what I want to achieve but my basic foundation of JS is not good enough to implement any of this. Trying to figure this out on my own without a lot of external help was next to impossible. Spending hours googling, going over the workshops and reading the JS book I bought was not sufficient and for my own mental health I did not want to continue as it was making me feel terrible. I am hopeful with more exposure I can get better and I will continue reading the book on JS I got which has been very helpful in solidifying some concepts but I have only covered the first two chapters. I am going to leave the cookie clicker here for now.
-
+I have a lot of gaps in my understanding of JS, this makes it really difficult to know how to start, comments and instructions given in the skeleton where useful to an extent but for some aspects I had too many gaps in my understanding to complete them. I know what I want to achieve but my basic foundation of JS is not good enough to implement any of this. Trying to figure this out on my own without a lot of external help was next to impossible. Spending hours googling, going over the workshops and reading the JS book I bought was not sufficient and for my own mental health I did not want to continue as it was making me feel terrible. I had to take lots of breaks and try get the right mindset again to try again. I am hopeful with more exposure I can get better and I will continue reading the book on JS I got which has been very helpful in solidifying some concepts but I have only covered the first two chapters.
 🏹 Feel free to add any other reflections you would like to share about your submission, for example:
 
 Requesting feedback about a specific part of your submission.
--- app.js
+++ app.js
@@ -23,20 +23,23 @@ resetButton.addEventListener("click", resetCookieCount);
 
 function resetCookieCount() {
   cookieCount = 0;
+  cookiesPerSecond = 1;
+  displayCookiesPerSecondValue();
   displayCookieCountValue();
+  console.log(cookiesPerSecond);
   console.log(cookieCount);
   saveLocalStorage();
 }
 
 function displayCookiesPerSecondValue() {
   cookiesPerSecondValue.textContent = cookiesPerSecond;
-  if (cookiesPerSecond == 1) {
-    cookieWord.textContent = "Cookie";
-  } else {
-    cookieWord.textContent = "Cookies";
-  }
+  // if (cookiesPerSecond == 1) {
+  //   cookieWord.textContent = "Cookie";
+  // } else {
+  //   cookieWord.textContent = "Cookies";
+  // }
 }
-//why does my if statement not work above?- changed cookiesPerSecondValue to cookiesPerSecond and then it worked but needed to add ${cookiesPerSecond} to new text content - maybe this will result in issues later when I change the value of cookiesPerSecond later? ok this definitely messed up my cookiesPerSecond value - when trying to style in css I noticed it's no longer there.
+//why does my if statement not work above?- changed cookiesPerSecondValue to cookiesPerSecond and then it worked but needed to add ${cookiesPerSecond} to new text content - maybe this will result in issues later when I change the value of cookiesPerSecond later? ok this definitely messed up my cookiesPerSecond value - when trying to style in css I noticed it's no longer there. I removed the if statement in this  function as it was breaking my setInterval() and resetCookiCount().
 
 function displayCookieCountValue() {
   cookieCountValue.textContent = cookieCount;
@@ -51,7 +54,6 @@ function increaseCookiesByCookiesPerSecond() {
 }
 
 setInterval(increaseCookiesByCookiesPerSecond, 1000);
-
 //this function logs the amount of times the user clicks the cookie button/image and displays it on the html
 function handleClick() {
   cookieCount = cookieCount + 1;
@@ -62,12 +64,23 @@ function handleClick() {
 
 cookieButton.addEventListener("click", handleClick);
 
+//this function was such a rollercoaster, it was so hard to see if it was working or not. A lot of typos and syntax errors intially meant I thought I had the logic completely wrong but had no real way of checking why as it wasn't showing up in the console. Using the debugger tool helped a little but it was very hard to figure out first the logic for this then debug it.
 function handleBuyClick(event) {
   console.log("buy!");
-  console.log(event.target.id);
+  console.log("id", event.target.id);
   let buttonId = event.target.id;
-  return buttonId;
-
+  let object = getObjectWithId(buttonId);
+  console.log("object", object); // testing if this works or not
+  console.log(object.cost);
+  console.log(object.increase); //testing
+  if (cookieCount == object.cost || cookieCount > object.cost) {
+    cookiesPerSecond = cookiesPerSecond + object.increase;
+    cookieCount = cookieCount - object.cost;
+  } else {
+    alert("You do not have enough cookies to buy this!");
+  }
+  displayCookiesPerSecondValue();
+  displayCookieCountValue();
   // Step 1: Get the id of the button that was clicked
   // let buttonId = ...
 
@@ -75,23 +88,32 @@ function handleBuyClick(event) {
   //         (call a function created outside this one - suggestion: call it getObjectWithId(id))
   // let object = ...
 
-  // Step 3: Use the object to add `increase` to fliesPerSecond (if the user has enough cookieCount)
+  // Step 3: Use the object to add `increase` to CookiesPerSecond (if the user has enough cookieCount)
   //         If they did, also subtract the `cost` from cookieCount
 
   // Step 4: Update the text of cookieCountContainer and fliesPerSecondContainer
 }
 
+// shopUpgrades = [
+//  {
+//     id: "1",
+//  },
+//  {
+//     id: "2",
+//  }
+// ]
+
 function getObjectWithId(id) {
-  for (let i = 0; i < shopUpgrades.length; id++) {
-    let shopUpgrade = shopUgrades[i];
-    console.log(shopUgrade.id);
-    if (shopUpgrade.id == buttonId) {
+  for (let i = 0; i < shopUpgrades.length; i++) {
+    let shopUpgrade = shopUpgrades[i];
+    console.log("I AM IN THIS FUNCTION", shopUpgrade.id);
+    if (shopUpgrade.id == id) {
       return shopUpgrade;
-    } else return "Unknown";
+    }
   }
+  return "Unknown";
 }
 
-//   // TODO: complete this function
 // this function should use the shopUpgrades array and return an object from the array
 // if its id is the same as the id passed in as a parameter to this function
 // hint: you will need a for loop and ifs / elses
@@ -99,22 +121,27 @@ function getObjectWithId(id) {
 // }
 
 //this function uses the data from the API and renders it onto the page
+//I did this with help from Joe, took me a while to figure out what it was doing but got there in the end.
 function renderShopUpgrades() {
   shopUpgrades.forEach((item) => {
+    let mainDiv = document.createElement("div");
+    mainDiv.className = "main-div";
     let makeDiv = document.createElement("div");
     let buyButton = document.createElement("button");
+    buyButton.className = "buy-button";
     buyButton.textContent = "Buy";
     buyButton.addEventListener("click", handleBuyClick);
     buyButton.id = `${item.id}`;
     makeDiv.textContent = `${item.name}, 
     COST: ${item.cost}C, 
-    gives you ${item.increase}Cookies Per Second`;
+    gives you ${item.increase}CPS`;
     makeDiv.className = "upgrades-box";
-    shopUpgradesContainer.appendChild(makeDiv);
-    shopUpgradesContainer.appendChild(buyButton);
+    mainDiv.appendChild(makeDiv);
+    mainDiv.appendChild(buyButton);
+    shopUpgradesContainer.appendChild(mainDiv);
   });
 }
-//how can I style individual elements in the makeDiv? Like the item.name and item.cost?
+//how can I style individual elements in the makeDiv? Like the item.name and item.cost? I was finally able to do this by realising you can use classNames made in JS in CSS!
 
 //this function fetches the data from the API and console logs it
 async function getShopUpgrades() {
@@ -146,13 +173,14 @@ function loadLocalStorage() {
   }
 
   const data = JSON.parse(retrieveData);
-
+  //this bit of logic was suprisingly hard to work out!
   cookieCount = data.cookieCount;
   cookiesPerSecond = data.cookiesPerSecond;
 }
 
 loadLocalStorage();
-
+displayCookiesPerSecondValue();
+displayCookieCountValue();
 //this function makes  getShopUgrades() wait before
 async function main() {
   await getShopUpgrades();
